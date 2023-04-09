77b0d3a5a6a3284c5ed9ae5e91286017fa85ddca
Tessa Isola <tessai159@icloud.com>
2023-04-09 12:55:14 -0500

    Started tracking testing modules. I know they're bad.

---
+++ test.py
@@ -0,0 +1,82 @@
+import unittest
+from Genetics_Toolbox import read_fasta_data, read_uniprot, n_glycosylation, Sequence
+from Genetics_Toolbox import generate_random_sequences
+
+sequences = read_fasta_data('testdata.data', '', update=True)
+basic_test_sequence = Sequence('AGCTGGCTA')
+
+class TestSequenceUpdates(unittest.TestCase):
+    def runTest(self):
+        update_test_sequence = generate_random_sequences(1, (0.0,1.0), (10**5, 10**6))
+        update_test_sequence.__update__()
+
+class TestPeptideChains(unittest.TestCase):
+    def runTest(self):
+        update_test_sequence = generate_random_sequences(1, (0.0,1.0), (10**5, 10**6))
+        update_test_sequence.__form_peptide_chain__()
+        update_test_sequence.__form_reverse_complement_peptide_chain__()
+        
+            
+class TestGCContent(unittest.TestCase):
+    def runTest(self):
+        for i, sequence in enumerate(sequences):
+            if i == 0:
+                gc = .476
+            elif i == 1:
+                gc = .67
+            elif i == 2:
+                gc = .508
+            elif i == 3:
+                gc = .622
+            elif i == 4:
+                gc = .611
+
+            self.assertEqual(float(sequence.gc_content(3)), gc,
+                             'GC content is incorrect')
+
+class TestBaseCounts(unittest.TestCase):
+    def runTest(self):
+        for i, sequence in enumerate(sequences):
+            if i == 0:
+                dna_base_count = {'G': 1019,
+                                  'C': 1050,
+                                  'T': 1122,
+                                  'A': 1159}
+            elif i == 1:
+                dna_base_count = {'G': 1494,
+                                  'C': 1406,
+                                  'T': 734,
+                                  'A': 692}
+            elif i == 2:
+                dna_base_count = {'G': 1261,
+                                  'C': 1282,
+                                  'T': 1235,
+                                  'A': 1231}
+            elif i == 3:
+                dna_base_count = {'G': 2270,
+                                  'C': 2242,
+                                  'T': 1358,
+                                  'A': 1383}
+            elif i == 4:
+                dna_base_count = {'G': 2499,
+                                  'C': 2499,
+                                  'T': 1620,
+                                  'A': 1558}
+                
+            self.assertEqual(dna_base_count, sequence.dna_base_count,
+                             'DNA base count is incorrect.')
+
+class TestSubstrings(unittest.TestCase):
+    def runTest(self):
+        subs = basic_test_sequence.find_substrings(8, 9)
+        self.assertEqual(subs, {'AGCTGGCT': 1, 'GCTGGCTA': 1, 'AGCTGGCTA': 1},
+                         "Substrings are incorrect")
+
+class TestProteinCreation(unittest.TestCase):
+    def runTest(self):
+        protein = read_uniprot(["B5ZC00"])[0]
+        self.assertEqual(protein.find_motifs(n_glycosylation),
+                    [85, 118, 142, 306, 395],
+                    "Protein motifs are incorrect")
+
+unittest.main()
